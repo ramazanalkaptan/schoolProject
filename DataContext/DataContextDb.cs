225042ac46df4b912652ca2822a9dd210074782d
Ramazan Alkaptan <133654582+ramazanalkaptan@users.noreply.github.com>
2024-10-22 11:19:03 +0300

    Add files via upload

---
+++ DataContext/DataContextDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Models;
+
+namespace School.DataContext
+{
+    public class DataContextDb : DbContext
+    {
+        public DataContextDb(DbContextOptions<DataContextDb>options):base(options)
+        {
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<AdminLogin> AdminLogins { get; set; }
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Register> Registers { get; set; }
+        public DbSet<UserForgotPassword> UserForgotPasswords { get; set; }
+    }
+}
