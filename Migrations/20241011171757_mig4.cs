5809d6cf4589bb5619dc7de780c746001bf576db
Ramazan Alkaptan <133654582+ramazanalkaptan@users.noreply.github.com>
2024-10-22 11:19:30 +0300

    Add files via upload

---
+++ Migrations/20241011171757_mig4.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserForgotEmail",
+                table: "UserForgotPasswords",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserForgotEmail",
+                table: "UserForgotPasswords");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserForgotPasswordId",
+                table: "Registers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registers_UserForgotPasswordId",
+                table: "Registers",
+                column: "UserForgotPasswordId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Registers_UserForgotPasswords_UserForgotPasswordId",
+                table: "Registers",
+                column: "UserForgotPasswordId",
+                principalTable: "UserForgotPasswords",
+                principalColumn: "Id");
+        }
+    }
+}
